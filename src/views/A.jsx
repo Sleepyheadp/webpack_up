0dccaed6d0f5984e0a443240198946e702503c91
sleepyheadp <17863965581@163.com>
2023-07-01 11:51:34 +0800

    2023-07-01：sync

--- src/views/A.jsx
+++
@@ -1,7 +0,0 @@
-let num = 1;
-const A = () => {
-	console.log("A", num);
-	num++;
-	return <div>A</div>;
-};
-export default A;
--- src/views/B.jsx
+++
@@ -1,7 +0,0 @@
-let num = 1;
-const B = () => {
-	console.log("B", num);
-	num--;
-	return <div>B</div>;
-};
-export default B;
